9a7b4bcf28ea7e6b9e04d41de0367e284e35f167
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-05-13 10:22:14 -0300

    Add files via upload

---
+++ source/GarcelloState.hx
@@ -0,0 +1,46 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class GarcelloState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	public static var needVer:String = "IDFK LOL";
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,        
+			"Thank you for downloading Release - Retrospecter remix!
+			 This mod is slighty flashy in some areas so,
+			 please proceed with caution
+			 ",
+			48);
+		txt.setFormat("VCR OSD Mono", 48, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
